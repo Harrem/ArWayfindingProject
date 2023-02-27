0da35d4960e497494dda9eea5bcd8708be84e976
Harrem <52822616+Harrem@users.noreply.github.com>
2023-02-27 22:00:03 +0300

    added method to calculation distance from target

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,17 +1,18 @@
 using UnityEngine;
 using UnityEngine.AI;
 
+
 public class Test : MonoBehaviour
 {
     private NavMeshPath path;
     private LineRenderer line;
     public Transform target;
-
     private void Start()
     {
         path = new NavMeshPath();
         line = transform.GetComponent<LineRenderer>();
         line.useWorldSpace = true;
+
     }
 
     private void Update()
@@ -20,5 +21,19 @@ public class Test : MonoBehaviour
             line.positionCount = path.corners.Length;
 
             line.SetPositions(path.corners);
+        Debug.Log("Distance: "+CalculatePath(path.corners));
+        
     }
+
+
+    private float CalculatePath(Vector3[] path)
+    {
+        float distance = 0f;
+        for(int i = 0; i< path.Length - 1; i++)
+        {
+            distance += Vector3.Distance(path[i], path[i+1]);
+        }
+        return distance;
+    }
+    
 }

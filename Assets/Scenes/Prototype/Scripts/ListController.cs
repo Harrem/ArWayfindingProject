4c70193669a77ac75e4d2ba81df7e33f76d10d3a
Harrem <52822616+Harrem@users.noreply.github.com>
2023-05-14 21:13:46 +0300

    updated qr codes

--- Assets/Scenes/Prototype/Scripts/ListController.cs
+++ Assets/Scenes/Prototype/Scripts/ListController.cs
@@ -22,35 +22,39 @@ public class ListController : MonoBehaviour
         int i = 0;
         foreach (var location in locations)
         {
-            // instantiate a new item prefab and set its parent to the content transform
-            GameObject newItem = Instantiate(itemPrefab, transform);
-
-            // set the text of the item
-            newItem.GetComponentInChildren<Text>().text = location.Name;
-            RectTransform itemTransform = newItem.GetComponent<RectTransform>();
-            itemTransform.anchoredPosition = new Vector2(0, -i * itemHeight);
-            itemTransform.anchorMin = new Vector2(0, 1);
-            itemTransform.anchorMax = new Vector2(1, 1);
-
-            // set the pivot to the top-center
-            itemTransform.pivot = new Vector2(0.5f, 1);
-
-            // set the left and right offsets to 10 pixels
-            itemTransform.offsetMin = new Vector2(0, itemTransform.offsetMin.y);
-            itemTransform.offsetMax = new Vector2(0, itemTransform.offsetMax.y);
-            // add an event listener to the item
-
-
-            newItem.GetComponentsInChildren<Image>()[1].enabled = false;
-
-            if (i % 2 == 1)
+            if (location.FloorNumber != 0)
             {
-                newItem.GetComponent<Image>().color = Color.grey;
+
+                // instantiate a new item prefab and set its parent to the content transform
+                GameObject newItem = Instantiate(itemPrefab, transform);
+
+                // set the text of the item
+                newItem.GetComponentInChildren<Text>().text = location.Name;
+                RectTransform itemTransform = newItem.GetComponent<RectTransform>();
+                itemTransform.anchoredPosition = new Vector2(0, -i * itemHeight);
+                itemTransform.anchorMin = new Vector2(0, 1);
+                itemTransform.anchorMax = new Vector2(1, 1);
+
+                // set the pivot to the top-center
+                itemTransform.pivot = new Vector2(0.5f, 1);
+
+                // set the left and right offsets to 10 pixels
+                itemTransform.offsetMin = new Vector2(0, itemTransform.offsetMin.y);
+                itemTransform.offsetMax = new Vector2(0, itemTransform.offsetMax.y);
+                // add an event listener to the item
+
+
+                newItem.GetComponentsInChildren<Image>()[1].enabled = false;
+
+                if (i % 2 == 1)
+                {
+                    newItem.GetComponent<Image>().color = Color.grey;
+                }
+                newItem.GetComponent<Button>().onClick.AddListener(() => OnItemClick(location));
+                i++;
+                contentTransform.sizeDelta = new Vector2(contentTransform.sizeDelta.x, items.Count * itemHeight);
             }
-            newItem.GetComponent<Button>().onClick.AddListener(() => OnItemClick(location));
-            i++;
         }
-        contentTransform.sizeDelta = new Vector2(contentTransform.sizeDelta.x, items.Count * itemHeight);
     }
 
     void OnItemClick(Target item)
